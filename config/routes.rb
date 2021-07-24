41d41c430e05928f91dbb3a97bfb50cec64db665
Ellezique <gizellevanzyl@gmail.com>
2021-07-25 07:28:12 +1000

    changed routing to localost/api

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,17 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  #CARD API: This rails api sends data from the postgress database to the react front end. This is seperate from the third party rapid api.
-  scope '/cardapi' do #http://localhost:3000/cardapi/cards
+ 
+  scope '/api' do #http://localhost:3000/api
+    #CARD API: This rails api sends data from the postgress database to the react front end. http://localhost:3000/api/cards This is seperate from the third party rapid api.
     get '/cards', to: 'cards#index'
     post '/cards', to: 'cards#create'
     # If using cards/param, then enter that above cards/:id
     get '/cards/:id', to: 'cards#show'
     put '/cards/:id', to: "cards#update"
     delete '/cards/:id', to: "cards#destroy"
-  end
-  #MESSAGES API: This rails api sends data from the postgress database to the react front end.
-  scope '/messageapi' do #http://localhost:3000/messageapi/messages
+ 
+  #MESSAGES API: This rails api sends data from the postgress database to the react front end http://localhost:3000/api/messages/
     get '/messages', to: 'messages#index'
     post '/messages', to: 'messages#create'
     # If using messagess/param, then enter that above messages/:id
