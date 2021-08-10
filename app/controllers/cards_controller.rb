87cb6944880c0d86d45b966b1bfa67487570e77d
Ellezique <gizellevanzyl@gmail.com>
2021-08-10 21:40:33 +1000

    tidy up code and remove unnecessary comments from controllers

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -24,17 +24,6 @@ class CardsController < ApplicationController
 
   # POST /cards or /cards.json
   def create
-    # @card = Card.new(card_params)
-  #   respond_to do |format|
-  #     if @card.save
-  #       format.html { redirect_to @card, notice: "Movie/Series Card was successfully created." }
-  #       format.json { render :show, status: :created, location: @card }
-  #     else
-  #       format.html { render :new, status: :unprocessable_entity }
-  #       format.json { render json: @card.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
   @card = Card.create(card_params)
     if @card.errors.any?
       render json: @card.errors, status: :unprocessable_entity
@@ -43,22 +32,8 @@ class CardsController < ApplicationController
     end
   end
 
-  # private 
-  # def card_params
-  #   params.require(:card).permit(:imdb_id)
-  # end
-
   # PATCH/PUT /cards/1 or /cards/1.json
   def update
-    # respond_to do |format|
-    #   if @card.update(card_params)
-    #     format.html { redirect_to @card, notice: "Movie/Series Card was successfully updated." }
-    #     format.json { render :show, status: :ok, location: @card }
-    #   else
-    #     format.html { render :edit, status: :unprocessable_entity }
-    #     format.json { render json: @card.errors, status: :unprocessable_entity }
-    #   end
-    # end
     @card.update(card_params)
     if @card.errors.any?
       render json: @card.errors, status: :unprocessable_entity
@@ -69,28 +44,22 @@ class CardsController < ApplicationController
 
   # DELETE /cards/1 or /cards/1.json
   def destroy
-    # @card.delete
-    # respond_to do |format|
-    #   format.html { redirect_to cards_url, notice: "Movie/Series Card was successfully deleted." }
-    #   format.json { head :no_content }
-    # end
     @card.delete
-    render json: {messsage: "Message deleted"}, status: 204
+    render json: {messsage: "Movie/Series Card was successfully deleted."}, status: 204
   end
 
- 
-    # Find and set card by id
-    def set_card  # not private: taken out of private for accessibility
-      begin
-        @card = Card.find(params[:id])
-      rescue
-        render json: {error: "The movie or series that you are looking for does not exist "}, status: 404
-      end
+  # Find and set card by id
+  def set_card  # not private: taken out of private for accessibility
+    begin
+      @card = Card.find(params[:id])
+    rescue
+      render json: {error: "The movie or series that you are looking for does not exist "}, status: 404
     end
+  end
 
-    private 
-    # Only allow a list of trusted parameters through.
-    def card_params #Card params are set to private to prevent access from the outside
-      params.require(:card).permit(:imdb_id)
-    end
+  private 
+  # Only allow a list of trusted parameters through.
+  def card_params #Card params are set to private to prevent access from the outside
+    params.require(:card).permit(:imdb_id)
+  end
 end
