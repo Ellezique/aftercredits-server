58cc25133f0edd3f24578c96b0c36058535ff1c2
Ellezique <gizellevanzyl@gmail.com>
2021-08-10 18:07:26 +1000

    Create card, message and user factories for factory bot, create Cards API test for success status and returning 4 objects created by factory

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
-# RSpec.describe "Messages", type: :request do
-#   describe "GET /index" do
-#     pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "Messages", type: :request do
+  describe "GET /index" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+end
+
+# describe 'Messages API', type: :request do
+#   it 'returns all messages' do
+#     FactoryBot.create(:message, m_text: "Dev themes are so entertaining", user_id: 1, card_id: 1)
+#     FactoryBot.create(:message, m_text: "Best movie ever!", user_id: 1, card_id: 1)
+#     FactoryBot.create(:message, m_text: "Im a test bot ..beep beep", user_id: 2, card_id: 1)
+#     get '/api/messages/'
+#     #test for a successful 200 response
+#     expect(response).to have_http_status(:success)
+#     #expect response body to return 3 message objects as created by factory bot
+#     # expect(JSON.parse(response.body).size).to eq(3)
 #   end
 # end
 
-describe 'Cards API', type: :request do
-  it 'returns all Cards' do
-    FactoryBot.create(:card, imdb_id:"tt0470752")
-    FactoryBot.create(:card, imdb_id:"tt8134186")
-    FactoryBot.create(:card, imdb_id:"tt2543312")
-    FactoryBot.create(:card, imdb_id:"tt0133093")
-    get '/api/cards'
-    #test for a successful 200 response
-    expect(response).to have_http_status(:success)
-    #expect response body to return 4 card objects as created by facotry bot
-    expect(JSON.parse(response.body).size).to eq(4)
-  end
-end
+
