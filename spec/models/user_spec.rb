f169591933ab2417443f4a3eb77fbbdce64b0eef
Ellezique <gizellevanzyl@gmail.com>
2021-08-12 14:55:34 +1000

    Added controller tests for user and message - errors for message test persist

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+    subject {
+      described_class.new(
+        username: "Fakeuser", 
+        email: "fakeuser@email.com", 
+        password: "FakePassword123456", 
+        password_confirmation: "FakePassword123456")
+    }
+
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+  
+    it "is not valid without a username" do
+      subject.username = nil
+      expect(subject).to_not be_valid
+    end
+    it "is not valid without an email" do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+  end
