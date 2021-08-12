fff4ade73249686a46694608a9f77299fb8d2ef4
Ellezique <gizellevanzyl@gmail.com>
2021-08-12 11:11:38 +1000

    Add factory user association

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-
+    username { "testuser" }
+    email { "test@email.com" }
+    password { "Password123456" } 
+    password_confirmation { "Password123456" }
   end
 end
