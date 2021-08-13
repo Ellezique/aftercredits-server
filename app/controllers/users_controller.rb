3746eec882ec2481e870099053c5def9b81a59fd
Chris Gibson <69493567+chris-gibs@users.noreply.github.com>
2021-08-13 12:57:08 +1000

    Adding isAdmin to sign_in response
    Adjusted sign_in route method response so client gets access to isAdmin boolean

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     # authenticate username and token
     if @user && @user.authenticate(params[:password])
       auth_token = Knock::AuthToken.new payload: {sub: @user.id}
-      render json: {username: @user.username, jwt: auth_token.token}, status: 201
+      render json: {username: @user.username, jwt: auth_token.token, isAdmin: @user.isAdmin}, status: 201
     else
       render json: {error: "username or password incorrect"}
     end
