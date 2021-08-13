456e6e8c29315f7a97aee6d7ece2599d28e845ac
Ellezique <gizellevanzyl@gmail.com>
2021-08-13 14:16:59 +1000

    tidied code

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Users API authentication", type: :request do
     FactoryBot.create(:user, username: "Uniqueuser1", email: "unique1@email.com", password: "UNPassword123456", password_confirmation: "UNPassword123456")
   end
   describe 'POST /api/auth/sign_in' do 
-    it 'existing user sign in' do
+    it 'lets existing user sign in' do
       post '/api/auth/sign_in', params: {user: {email: "unique1@email.com", password: "UNPassword123456", password_confirmation: "UNPassword123456" }}
       expect(response).to have_http_status(200) 
     end
