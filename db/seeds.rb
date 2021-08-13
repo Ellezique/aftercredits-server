e9a809d3ba9d0380570c04745ca9e88c6513cce1
Ellezique <gizellevanzyl@gmail.com>
2021-08-13 19:36:07 +1000

    change seeds file to just one admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,29 +14,29 @@
 #Add a card
 if Card.count == 0
   # exmachina tt0470752; devs tt8134186, halt and catch fire tt2543312, the matrix tt0133093
-  Card.create(imdb_id: "tt0470752")
-  Card.create(imdb_id: "tt8134186")
+  # Card.create(imdb_id: "tt0470752")
+  # Card.create(imdb_id: "tt8134186")
   # Card.create(imdb_id: "tt2543312")
   # Card.create(imdb_id: "tt0133093")
 end
 
 #Add Admin users
 if User.count == 0
-  User.create(username: "Gizelle", email: "gizelle205@hotmail.com", password: "Password123456", password_confirmation: "Password123456", isAdmin: true)
+  # User.create(username: "Gizelle", email: "gizelle205@hotmail.com", password: "Password123456", password_confirmation: "Password123456", isAdmin: true)
   User.create(username: "testuser", email: "test@email.com", password: "Password123456", password_confirmation: "Password123456", isAdmin: true)
 end
 
 #Add messages 
 if Message.count == 0
-   Message.create(m_text: "Dev themes are so entertaining", user_id: 1, card_id: 1)
-   Message.create(m_text: "Best movie ever!", user_id: 1, card_id: 1)
-   Message.create(m_text: "Im a test bot ..beep beep", user_id: 1, card_id: 2)
-   Message.create(m_text: "Nulla rhoncus volutpat massa.", user_id: 1, card_id: 2)
+  #  Message.create(m_text: "Dev themes are so entertaining", user_id: 1, card_id: 1)
+  #  Message.create(m_text: "Best movie ever!", user_id: 1, card_id: 1)
+  #  Message.create(m_text: "Im a test bot ..beep beep", user_id: 1, card_id: 2)
+  #  Message.create(m_text: "Nulla rhoncus volutpat massa.", user_id: 1, card_id: 2)
 
-   Message.create(m_text: "Donec nulla ipsum, facilisis id nunc non, auctor mollis sapien. Nullam sit amet neque bibendum, varius urna ut, placerat magna. Nullam sed hendrerit metus, sit amet pellentesque arcu. In et cursus risus. Quisque nisi dui, maximus quis purus finibus, rhoncus tempus erat.", user_id: 2, card_id: 1)
-   Message.create(m_text: "Mauris fermentum mollis enim id fringilla. Suspendisse elementum, nisl feugiat ultricies gravida, elit urna egestas nisi, ut auctor nunc mi nec magna.", user_id: 2, card_id: 1)
-   Message.create(m_text: "Sed id ornare sapien.", user_id: 2, card_id: 2)
-   Message.create(m_text: "Praesent eu risus porttitor, ultricies augue et, gravida velit. Pellentesque non elementum urna, sit amet sagittis eros.", user_id: 2, card_id: 2)
+  #  Message.create(m_text: "Donec nulla ipsum, facilisis id nunc non, auctor mollis sapien. Nullam sit amet neque bibendum, varius urna ut, placerat magna. Nullam sed hendrerit metus, sit amet pellentesque arcu. In et cursus risus. Quisque nisi dui, maximus quis purus finibus, rhoncus tempus erat.", user_id: 2, card_id: 1)
+  #  Message.create(m_text: "Mauris fermentum mollis enim id fringilla. Suspendisse elementum, nisl feugiat ultricies gravida, elit urna egestas nisi, ut auctor nunc mi nec magna.", user_id: 2, card_id: 1)
+  #  Message.create(m_text: "Sed id ornare sapien.", user_id: 2, card_id: 2)
+  #  Message.create(m_text: "Praesent eu risus porttitor, ultricies augue et, gravida velit. Pellentesque non elementum urna, sit amet sagittis eros.", user_id: 2, card_id: 2)
 end
 
 #Messages
