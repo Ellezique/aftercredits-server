a64a6975a828fdbf7691f513409f11d1da61e65b
Ellezique <gizellevanzyl@gmail.com>
2021-08-13 12:20:30 +1000

    In cards controller, added checkuser admin on destroy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    # 'localhost:3000', '127.0.0.1:3000' 
+    # 'localhost:3000', '127.0.0.1:3000' 'https://aftercredits.netlify.app', 'http://localhost:8000'
 
     resource '*',
       headers: :any,
