01d8151f87ff8ccd6d6a7080b6fbc09ade3b7c57
Ellezique <gizellevanzyl@gmail.com>
2021-08-13 13:45:43 +1000

    Add a user request test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "API authentication", type: :request do
-  
-
-
+RSpec.describe "Users API authentication", type: :request do
+  before(:all) do
+    FactoryBot.create(:user, username: "Uniqueuser1", email: "unique1@email.com", password: "UNPassword123456", password_confirmation: "UNPassword123456")
+  end
+  describe 'POST /api/auth/sign_in' do 
+    it 'existing user sign in' do
+      post '/api/auth/sign_in', params: {user: {email: "unique1@email.com", password: "UNPassword123456", password_confirmation: "UNPassword123456" }}
+      expect(response).to have_http_status(200) 
+    end
+  end
 end
