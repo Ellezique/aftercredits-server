2e30e89112225e98ac3ca552d3a69593c85f5e47
Ellezique <gizellevanzyl@gmail.com>
2021-08-13 11:16:32 +1000

    modified user spec file

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "API authentication", type: :request do
   
- # let!(:user) { FactoryBot.create(:user, username: "randomuser", email: "random@email.com", password: "ranPassword123456", password_confirmation: "ranPassword123456") }
 
 
 end
